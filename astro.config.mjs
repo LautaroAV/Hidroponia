57757f3feed1f5906cc884fecc92f70737317483
LautaroAV <lautyx027@gmail.com>
2024-06-08 18:41:29 -0300

    Cambios en Sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
+// https://astro.build/config
 export default defineConfig({
   // Other configurations...
 
-  output: 'server', // Change this to 'hybrid' if you need some pages to be static
+  output: 'server' // Change this to 'hybrid' if you need some pages to be static
+  ,
+  integrations: [icon()]
 });
